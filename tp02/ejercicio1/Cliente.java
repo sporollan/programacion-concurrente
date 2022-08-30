e0872e81729edc2616e76e190b7bc8e78ef35e25
sporollan <s.porollan@gmail.com>
2022-08-30 12:30:49 -0300

    add tp2 ejercicio 1

---
+++ tp02/ejercicio1/Cliente.java
@@ -0,0 +1,13 @@
+
+
+public class Cliente extends Thread {
+    public void run() {
+        System.out.println("soy" + Thread.currentThread().getName());
+        Recurso.uso();
+        try {
+            Thread.sleep(2000);
+
+        } catch(InterruptedException e) {
+        };
+    };
+}
---
+++ tp02/ejercicio1/Recurso.java
@@ -0,0 +1,7 @@
+public class Recurso {
+    static void uso(){
+        Thread t=Thread.currentThread();
+        System.out.println("en Recurso: Soy" + t.getName());
+    }
+}
+
