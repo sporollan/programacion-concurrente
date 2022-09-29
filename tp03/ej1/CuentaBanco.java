8476d14dde7d3c9c4009b7dd2e44bb78fbe490f5
sporollan <santiago.porollan@gmail.com>
2022-09-29 15:14:12 -0300

    add ej1

---
+++ tp03/ej1/CuentaBanco.java
@@ -0,0 +1,16 @@
+package tp03.ej1;
+
+public class CuentaBanco {
+    private int balance = 50;
+    public CuentaBanco(){
+
+    }
+
+    public int getBalance(){
+        return balance;
+    }
+
+    public void retiroBancario(int retiro){
+        balance = balance - retiro;
+    }
+}
---
+++ tp03/ej1/Main.java
@@ -0,0 +1,11 @@
+package tp03.ej1;
+
+public class Main {
+    public static void main(String[] args){
+        VerificarCuenta vc = new VerificarCuenta();
+        Thread Luis = new Thread(vc, "Luis");
+        Thread Manuel = new Thread(vc, "Manuel");
+        Luis.start();
+        Manuel.start();
+    }
+}
