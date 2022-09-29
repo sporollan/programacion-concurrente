13e03e290e9f4a961c6de5fd0fd1d4f4caba3bcd
sporollan <santiago.porollan@gmail.com>
2022-09-29 14:43:41 -0300

    add ej7

---
+++ tp02/ej7/Cajero.java
@@ -0,0 +1,41 @@
+package tp02.ej7;
+
+public class Cajero implements Runnable{
+    private String nombre;
+    private Cliente cliente;
+    private long initialTime;
+    public Cajero(String n){
+        this.nombre = n;
+    }
+    public Cajero(String n, Cliente c, long i){
+        this.nombre = n;
+        this.cliente = c;
+        this.initialTime = i;
+    }
+
+    public void run(){
+        this.procesarCompra(this.cliente, this.initialTime);
+    }
+
+    public void procesarCompra(Cliente cliente, long timeStamp) {
+        System.out.println("El cajero " + this.nombre + " COMIENZA A PROCESAR LA COMPRA DEL CLIENTE " + 
+        cliente.getNombre() + " EN EL TIEMPO: " + 
+        (System.currentTimeMillis() - timeStamp) / 1000 +
+        "seg");
+        
+        for( int i = 0; i < cliente.getCarroCompra().length; i++) {
+            this.esperarXsegundos(cliente.getCarroCompra()[i]);
+            System.out.println("Procesado el producto " + (i + 1) +
+            "->Tiempo: " + (System.currentTimeMillis() - timeStamp) /
+            1000 + "seg");
+        }
+        System.out.println("El cajero " + this.nombre + 
+        " HA TERMINADO DE PROCESAR " + cliente.getNombre() + " EN EL TIEMPO: " +
+         (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
+    }
+    public void esperarXsegundos(int x){
+        try{
+            Thread.sleep(x * 1000);}
+            catch(InterruptedException e){}
+    }
+}
---
+++ tp02/ej7/Cliente.java
@@ -0,0 +1,28 @@
+package tp02.ej7;
+
+public class Cliente {
+    private String nombre;
+    private int[] carroCompra;
+    
+    public Cliente(String n, int[] c){
+        this.nombre = n;
+        this.carroCompra = c;
+    }
+
+    public void setNombre(String n){
+        this.nombre = n;
+    }
+
+    public void setCarroCompra(int[] c){
+        this.carroCompra = c;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public int[] getCarroCompra(){
+        return this.carroCompra;
+    }
+
+}
